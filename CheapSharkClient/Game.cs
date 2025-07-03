4692b787ca72e4fcbb1a06282704de6400b167c9
timofey <timofeyperev@gmail.com>
2025-07-03 22:54:04 +0300

    added CheapSharkClient

---
+++ CheapSharkClient/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheapSharkClient
+{
+    internal class Game
+    {
+        public string cheapest { get; set; } = "";
+        public string external { get; set; } = "";
+    }
+}
---
+++ CheapSharkClient/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using CheapSharkClient;
+
+HttpClient httpClient = new HttpClient();
+
+while (true)
+{
+    Console.Write("Введи название игры или q для выхода\n");
+    string? keyword = Console.ReadLine();
+    if (keyword == "q")
+    {
+        break;
+    }
+
+    string url = $"https://www.cheapshark.com/api/1.0/games?title={keyword}&limit=5";
+
+    try
+    {
+        string json = await httpClient.GetStringAsync(url);
+        List<Game>? games = JsonSerializer.Deserialize<List<Game>>(json);
+        if (games == null || games.Count == 0)
+        {
+            Console.WriteLine("Ничего не найдено");
+        }
+
+        else
+        {
+            foreach (Game game in games)
+            {
+                Console.WriteLine($"Название: {game.external}\nЦена: {game.cheapest}$\n");
+            }
+        }
+    }
+
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Произошла ошибка: {ex.Message}");
+    }
+}
